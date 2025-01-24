aacad672c272468b5faeeeac2814abe1bb696b93
Jonathan Herrmann <jonorh@web.de>
2025-01-24 21:16:40 +0100

    Fix headlines

--- webapp/lib/main.dart
+++ webapp/lib/main.dart
@@ -13,12 +13,12 @@ class WeddingFormApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hochzeit von Katha und Jonathan',
+      title: 'Hochzeit von Sarah und Noah',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const WeddingFormHomePage(title: 'Hochzeit von Katha und Jonathan'),
+      home: const WeddingFormHomePage(title: 'Hochzeit von Sarah und Noah'),
     );
   }
 }
